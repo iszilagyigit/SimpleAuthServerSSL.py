561f226fc57074bb6ab821578563079536d66b6a
Istvan Sz <pty01isz@gmail.com>
2021-12-19 12:45:23 +0000

    updated for python3

--- SimpleHttpsAuthServer.py
+++ SimpleHttpsAuthServer.py
@@ -1,24 +1,34 @@
-import BaseHTTPServer
-from SimpleHTTPServer import SimpleHTTPRequestHandler
+#
+# Python3 script
+#
+import http.server
+import socketserver
+from http.server import SimpleHTTPRequestHandler
 import sys
 import os
 import base64
 import ssl
-import SocketServer
+
 
 key = ""
-CERTFILE_PATH = "/root/server.pem"
+
+#
+#  key and self signed certificate created with:
+#  openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
+#
+CERTFILE_PATH = "/home/ubuntu/https/cert.pem"
+KEYFILE_PATH= "/home/ubuntu/https/key.pem"
 
 class AuthHandler(SimpleHTTPRequestHandler):
     ''' Main class to present webpages and authentication. '''
     def do_HEAD(self):
-        print "send header"
+        print ("send header")
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
     def do_AUTHHEAD(self):
-        print "send header"
+        print ("send header")
         self.send_response(401)
         self.send_header('WWW-Authenticate', 'Basic realm=\"Test\"')
         self.send_header('Content-type', 'text/html')
@@ -27,40 +37,35 @@ class AuthHandler(SimpleHTTPRequestHandler):
     def do_GET(self):
         global key
         ''' Present frontpage with user authentication. '''
-        if self.headers.getheader('Authorization') == None:
+        if self.headers.get('Authorization') == None:
             self.do_AUTHHEAD()
-            self.wfile.write('no auth header received')
+            self.wfile.write('no auth header received'.encode('utf-8'))
             pass
-        elif self.headers.getheader('Authorization') == 'Basic '+key:
+        elif self.headers.get('Authorization') == 'Basic '+str(key,'utf-8'):
             SimpleHTTPRequestHandler.do_GET(self)
             pass
         else:
             self.do_AUTHHEAD()
-            self.wfile.write(self.headers.getheader('Authorization'))
-            self.wfile.write('not authenticated')
+            self.wfile.write(self.headers.get('Authorization'))
+            self.wfile.write('not authenticated'.encode('utf-8'))
             pass
 
 def serve_https(https_port=80, HandlerClass = AuthHandler,
-         ServerClass = BaseHTTPServer.HTTPServer):
-    httpd = SocketServer.TCPServer(("", PORT), HandlerClass)
-    httpd.socket = ssl.wrap_socket (httpd.socket, certfile=CERTFILE_PATH, server_side=True)
+         ServerClass = http.server.HTTPServer):
+    httpd = socketserver.TCPServer(("", https_port), HandlerClass)
+    httpd.socket = ssl.wrap_socket (httpd.socket, certfile=CERTFILE_PATH, keyfile=KEYFILE_PATH, server_side=True)
 
     sa = httpd.socket.getsockname()
-    print "Serving HTTP on", sa[0], "port", sa[1], "..."
+    print ("Serving HTTP on", sa[0], "port", sa[1], "...")
     httpd.serve_forever()
 
 if __name__ == '__main__':
     if len(sys.argv)<3:
-        print "usage SimpleAuthServer.py [port] [username:password]"
+        print ("usage SimpleAuthServer.py [port] [username:password]")
         sys.exit()
 
-	https_port = int(sys.argv[1])
-	key = base64.b64encode(sys.argv[2])
-
-	if len(sys.argv) == 4:
-		change_dir = sys.argv[3]
-		print "Changing dir to {cd}".format(cd=change_dir)
-		os.chdir(change_dir)
+    https_port = int(sys.argv[1])
+    key = base64.b64encode(sys.argv[2].encode('utf-8'))
 
     serve_https(https_port)
 
